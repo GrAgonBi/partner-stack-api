820cb0f273002c7437ffcb7817de9f6e2523e278
GrAgonBi <abiweiyang0913@gmail.com>
2024-03-21 23:53:01 -0400

    GET comments and POST new comment

--- controllers/partner-controller.js
+++ controllers/partner-controller.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
+const uniqid = require("uniqid");
 
 const JSON_FILE_PATH = "./data/partners.json";
 
 const getAllPartners = () => {
   const partners = JSON.parse(fs.readFileSync(JSON_FILE_PATH));
   return partners;
+};
+
+const setAllPartners = (partners) => {
+  const partnersJson = JSON.stringify(partners);
+  fs.writeFileSync(JSON_FILE_PATH, partnersJson);
 };
 
 const getPartners = (req, res) => {
@@ -25,15 +31,64 @@ const getPartners = (req, res) => {
 const getPartner = (req, res) => {
   const { id } = req.params;
   const allPartners = getAllPartners();
-  const partner = allPartners.filter((partner) => {
+  const partner = allPartners.find((partner) => {
     return partner.id === id;
   });
 
-  if (partner.length === 0) {
+  if (!partner) {
     return res.status(404).json("Message: No such partner");
   }
 
-  return res.status(200).json(partner[0]);
+  return res.status(200).json(partner);
 };
 
-module.exports = { getPartners, getPartner };
+const getComments = (req, res) => {
+  const { partnerId } = req.params;
+
+  const allPartners = getAllPartners();
+  const partner = allPartners.find((partner) => {
+    return partner.id === partnerId;
+  });
+
+  if (!partner) {
+    return res.status(404).json("Message: No such partner");
+  }
+
+  const comments = partner.comments;
+  res.status(200).json(comments);
+};
+
+const postComment = (req, res) => {
+  const { name, comment } = req.body;
+  const partnerId = req.params.partnerId;
+
+  if (!name || !comment) {
+    return res.status(400).json("Message: Please enter the required fields");
+  }
+
+  const newComment = { id: uniqid(), name, comment };
+
+  const allPartners = getAllPartners();
+  const partner = allPartners.find((partner) => {
+    return partner.id === partnerId;
+  });
+
+  if (!partner) {
+    return res.status(404).json("Message: No such partner");
+  }
+
+  const comments = [...partner.comments];
+  comments.unshift(newComment);
+
+  const updatedParters = allPartners.map((partner) => {
+    if (partner.id === partnerId) {
+      return { ...partner, comments };
+    }
+    return partner;
+  });
+
+  setAllPartners(updatedParters);
+  res.status(201).json(newComment);
+};
+
+module.exports = { getPartners, getPartner, getComments, postComment };
--- routes/partner.js
+++ routes/partner.js
@@ -2,10 +2,14 @@ const router = require("express").Router();
 const {
   getPartners,
   getPartner,
+  getComments,
+  postComment,
 } = require("../controllers/partner-controller");
 
 router.get("/", getPartners);
 
 router.get("/:id", getPartner);
+router.route("/:partnerId/comments").get(getComments).post(postComment);
+router.delete("/:partnerId/comments/:commentID", getPartner);
 
 module.exports = router;
